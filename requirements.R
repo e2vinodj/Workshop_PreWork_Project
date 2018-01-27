3fdea56a26a1af7d0f258f13dea9d56f8d42a111
unknown <e2vinodj@yahoo.com>
2018-01-27 14:14:51 -0600

    added workshop data files and modified notebook

--- requirements.R
+++ requirements.R
@@ -3,4 +3,14 @@
 # to install packages so that the right version is installed.
 
 # For example
-devtools::install_version('tidyverse', version = '1.2.1')
+# devtools::install_version('tidyverse', version = '1.2.0')
+
+devtools::install_version('data.table')
+devtools::install_version('dplyr')
+devtools::install_version('ggplot2')
+devtools::install_version('stringr')
+devtools::install_version('plotly')
+devtools::install_version('lubridate')
+devtools::install_version('survival')
+devtools::install_version('broom')
+devtools::install_version('eha')
